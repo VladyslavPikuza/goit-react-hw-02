18668ea66fb84aee5de326b8e54318d01b826652
Vladyslav <168086670+VladyslavPikuza@users.noreply.github.com>
2024-10-29 12:46:51 +0200

    preparind

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import Description from "./Description/Description";
+import Feedback from "./Feedback/Feedback";
+import Options from "./Options/Options";
+
+
+const App = () => {
+  return (
+    <div>
+      <Description/>
+      <Options/>
+      <Feedback/>
+    </div>
+  )
+};
+
+export default App;
+
+
+
+
+
